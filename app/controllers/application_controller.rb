d120f60c7975ad132a7a6e40a0876d91cf9422d6
Seth Blanchard <nsethblanchard@gmail.com>
2021-01-11 09:08:57 -0500

    removed some from welcome.erb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,8 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "password123 is the most secure"
   end
 
   get "/" do
